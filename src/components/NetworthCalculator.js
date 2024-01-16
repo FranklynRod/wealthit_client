111345f6f822ba2a24caaee1be5d1424ece63a45
Franklyn Rodriguez <92125381+FranklynRod@users.noreply.github.com>
2024-01-15 21:37:30 -0500

    added to UI

--- src/components/NetworthCalculator.js
+++ src/components/NetworthCalculator.js
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const NetworthCalculator = () => {
   return (
-   
     <div>
-        <h2>Networth Calculator</h2>
-    <form>
+      <h2>Networth Calculator</h2>
+      <form>
         <label for="cars">Asset Description</label>
         <select id="assets" name="assets">
-        <option value="cash">Cash</option>
-        <option value="retirement">Retirement</option>
-        <option value="investment">Investment</option>
-        <option value="equity">Home Equity</option>
-        <option value="other">Other</option>
+          <option value="cash">Cash</option>
+          <option value="retirement">Retirement</option>
+          <option value="investment">Investment</option>
+          <option value="equity">Home Equity</option>
+          <option value="other">Other</option>
         </select>
         <input type="text" name="other" id="other"></input>
         Asset Amount
@@ -20,21 +19,21 @@ const NetworthCalculator = () => {
         <button>Add</button>
         <label for="cars">Liability Description</label>
         <select id="liability" name="liability">
-        <option value="cash">Loan</option>
-        <option value="retirement">Credit Card</option>
-        <option value="investment">Klarna</option>
-        <option value="equity">Afterpay</option>
-        <option value="other">Other</option>
+          <option value="loan">Loan</option>
+          <option value="credit-card">Credit Card</option>
+          <option value="Klarna">Klarna</option>
+          <option value="Afterpay">Afterpay</option>
+          <option value="Other">Other</option>
         </select>
-        <input type="text" name="other" id="other"></input>
+        { document.getElementById("liability")?.value === "Other" && <input type="text" name="other" id="other"> </input>}
+    
         Liability Amount
         <input></input>
         <button>Add</button>
         <button>Calculate</button>
-    </form>
+      </form>
     </div>
+  );
+};
 
-  )
-}
-
-export default NetworthCalculator
+export default NetworthCalculator;
