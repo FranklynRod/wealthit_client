54211a6a412e11a44b51f3d7b58d6f4d2e51a104
Franklyn Rodriguez <92125381+FranklynRod@users.noreply.github.com>
2024-05-03 07:40:17 -0400

    set up signin page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useContext, useState, useRef} from 'react';
+import { useLocation, useNavigation, Link } from 'react-router-dom';
+import UserContext from '../context/UserContext';
+import ErrorDisplay from './ErrorDisplay';
+
 
 const SignIn = () => {
+ 
+  //State
+  const username = useRef(null);
+  const password = useRef(null);
+  const [errors, setErrors] = useState([]);
+
+   //Variables
+   const location = useLocation();
+   const navigate = useNavigation;
+   const {actions} = useContext(UserContext);
+   const credentials = {
+     username: username.current.value, 
+     password: password.current.value,   
+   }
+  
+  //Event Handler
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let from = '/'
+    if (location.state)
+      from = location.state.from;
+    
+    try{
+      const user = await actions.signIn(credentials)
+      if(user){
+        navigate(from);
+      }else{
+        setErrors(["Sign-in was unsuccessful"])
+      }
+    }catch(error){
+      navigate('/error')
+    }
+  }
   return (
-    <><h1>
+    <main>
+      <h1>
     Sign in
-</h1>
-<form>
-    <label >First Name:</label>
-    <input aria-label='first-name-input'></input>
-    <label>Last Name:</label>
-    <input aria-label='last-name-input'></input>
+     </h1>
+
+<form onSubmit={handleSubmit}>
+    <label >UserName:</label>
+    <input aria-label='username-input'></input>
     <label>Password</label>
     <input aria-label='password-input'></input>
     <label>Password Again</label>
     <input aria-label='password-input'></input>
 </form>
-<button className='signin-btn'>Submit</button>
+<button className='signin-btn' >Submit</button>
 <p>Don't have a user account? Click here to <Link to="/signup">sign up</Link>!</p> 
-</>
+</main>
   )
 }
 
